97ff80f3f9cbc70462e1dcbe2694cd5ecd9bb167
Grafart13 <dpjarosz@gmail.com>
2014-06-14 01:29:19 +0200

    Basic Class

--- src/heart/Production.java
+++ src/heart/Production.java
@@ -16,8 +16,14 @@ public class Production {
      //   parseLine(line);
         left = new Symbol(line.charAt(0));
         if ((Character.compare(line.charAt(1), '-')==1) && (Character.compare(line.charAt(2), '>')==1)) {
-
+            int key=0;
+            int i=3;
+            char sign = line.charAt(i);
+            while (Character.compare(sign, '\n')==0) {
+                right.put(key, new Symbol(sign));
+                key++;
+                sign = line.charAt(++i);
+            }
         }
-
     }
 }
